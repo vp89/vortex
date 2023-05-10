34762ab17394ab32421cdfc43d34105c5323b0a6
Vince Pergolizzi <pergolizzivincenzo@gmail.com>
2023-05-10 09:54:35 -0400

    improve print debugging

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ using System.Text.Json.Serialization;
 
 while (true) {
     var message = Console.ReadLine();
-    Console.Error.WriteLine($"Read the following {message}");
+    Console.Error.WriteLine($"Message received: {message}");
     HandleMessageAsync(message);
 }
 
@@ -11,16 +11,34 @@ async void HandleMessageAsync(string message) {
     try {
         var deserializedMessage = JsonSerializer.Deserialize<MaelstromMessage>(message);
 
+        // build reply code
         if (deserializedMessage?.Body is null) return;
 
-        var source = deserializedMessage.Source;
-        deserializedMessage.Source = deserializedMessage.Destination;
-        deserializedMessage.Destination = source;
-        deserializedMessage.Body["type"] += "_ok";
-        deserializedMessage.Body["in_reply_to"] = deserializedMessage.Body["msg_id"];
+        var clonedBody = new Dictionary<string, object>(deserializedMessage.Body);
 
-        var serializedMessage = JsonSerializer.Serialize(deserializedMessage);
-        Console.WriteLine(serializedMessage);
+        var messageType = clonedBody["type"];
+        var messageId = clonedBody["msg_id"];
+
+        Console.Error.WriteLine(
+            $"Received message {messageId} of type {messageType} from {deserializedMessage.Source}");
+
+        var replyMessageId = Interlocked.Increment(ref Globals.MESSAGE_ID);
+        var replyMessageType = messageType += "_ok";
+
+        clonedBody["msg_id"] = replyMessageId;
+        clonedBody["type"] = replyMessageType;
+        clonedBody["in_reply_to"] = deserializedMessage.Body["msg_id"];
+
+        var reply = new MaelstromMessage()
+        {
+            Source = deserializedMessage.Destination,
+            Destination = deserializedMessage.Source,
+            Body = clonedBody
+        };
+
+        var serializedReply = JsonSerializer.Serialize(reply);
+        Console.Error.WriteLine($"Sending message {replyMessageId} of type {replyMessageType} as reply: {serializedReply}");
+        Console.WriteLine(serializedReply);
     } catch (Exception e) {
         Console.Error.WriteLine($"ERROR - {e.Message}");
     }
@@ -33,7 +51,16 @@ class MaelstromMessage
 
     [JsonPropertyName("dest" )]
     public string Destination { get; set; }
+    
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
 
     [JsonPropertyName("body")]
     public Dictionary<string, object> Body { get; set; }
+}
+
+
+static class Globals
+{
+    public static int MESSAGE_ID = 0;
 }
--- run-echo.sh
+++ run-echo.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 dotnet build
-./maelstrom/maelstrom test -w echo --bin bin/Debug/net6.0/vortex --node-count 1 --time-limit 10
+./maelstrom/maelstrom test -w echo --bin bin/Debug/net6.0/vortex --node-count 1 --time-limit 3 --log-stderr
