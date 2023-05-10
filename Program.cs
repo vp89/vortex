ea0d5a14c9b1c78e47059943c0027b59317a2fa6
Vince Pergolizzi <pergolizzivincenzo@gmail.com>
2023-05-10 09:29:47 -0400

    echo workload passes

--- Program.cs
+++ Program.cs
@@ -1,1 +1,39 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+while (true) {
+    var message = Console.ReadLine();
+    Console.Error.WriteLine($"Read the following {message}");
+    HandleMessageAsync(message);
+}
+
+async void HandleMessageAsync(string message) {
+    try {
+        var deserializedMessage = JsonSerializer.Deserialize<MaelstromMessage>(message);
+
+        if (deserializedMessage?.Body is null) return;
+
+        var source = deserializedMessage.Source;
+        deserializedMessage.Source = deserializedMessage.Destination;
+        deserializedMessage.Destination = source;
+        deserializedMessage.Body["type"] += "_ok";
+        deserializedMessage.Body["in_reply_to"] = deserializedMessage.Body["msg_id"];
+
+        var serializedMessage = JsonSerializer.Serialize(deserializedMessage);
+        Console.WriteLine(serializedMessage);
+    } catch (Exception e) {
+        Console.Error.WriteLine($"ERROR - {e.Message}");
+    }
+}
+
+class MaelstromMessage
+{
+    [JsonPropertyName("src")]
+    public string Source { get; set; }
+
+    [JsonPropertyName("dest" )]
+    public string Destination { get; set; }
+
+    [JsonPropertyName("body")]
+    public Dictionary<string, object> Body { get; set; }
+}
--- run-echo.sh
+++ run-echo.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+dotnet build
 ./maelstrom/maelstrom test -w echo --bin bin/Debug/net6.0/vortex --node-count 1 --time-limit 10
